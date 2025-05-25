acfd35e6407e862173134df87c4447b0db20c991
Mohammed <88824957+m7medVision@users.noreply.github.com>
2025-05-25 15:15:17 +0400

    feat: add background audio to main application for enhanced user experience

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <audio
+      src='bg.mp3'
+      loop
+      preload="auto"
+      autoPlay
+    />
     <App />
   </StrictMode>,
 )
